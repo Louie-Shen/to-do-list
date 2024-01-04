08bcff4aeef578bd1e034b9fd601e8798247f948
Louie-Shen <71159008+Louie-Shen@users.noreply.github.com>
2024-01-03 21:42:55 -0500

    Checkbox logic added

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TaskItem = ({ task }) => {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
+
     return (
-        <ul><input type="checkbox"/>{task}</ul>
+        <ul>
+            <input type="checkbox" 
+            checked={isChecked}
+            onChange={handleCheckboxChange}/>
+            {task}
+            <p>{isChecked ? 'Checked' : 'Not Checked'}</p></ul>
+        
     );
 };
 
