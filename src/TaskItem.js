5747b7d39db0ff027ced4566d00d660819958525
Louie-Shen <71159008+Louie-Shen@users.noreply.github.com>
2024-01-06 16:16:57 -0500

    dropdown menu for options without functionality

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 const TaskItem = ({ task }) => {
 
+    // Checking if checkbox is checked
     const [isChecked, setIsChecked] = useState(false);
 
     const handleCheckboxChange = () => {
@@ -11,13 +12,33 @@ const TaskItem = ({ task }) => {
 
     const listItemClasses = isChecked ? 'listItem checked' : 'listItem notChecked'
 
+
+    // Checking if options button is pressed to drop down menu
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownVisible(!dropdownVisible);
+    };
+
+    const closeDropdown = () => {
+        setDropdownVisible(false);
+    };
+
+
     return (
         <ul className = {listItemClasses}>
             <input type="checkbox" 
             checked={isChecked}
             onChange={handleCheckboxChange}/>
             {task}
-            <p>{isChecked ? 'Checked' : 'Not Checked'}</p>
+            <form>
+                <label className = "options" for='options'>Extra Options:</label>
+                <select className = "options" id='options' name='extraOptions'>
+                    <option value="Due Date">Due Date</option>
+                    <option value="Time Needed">Time Needed</option>
+                </select>
+            </form>
+            
         </ul>
         
     );
